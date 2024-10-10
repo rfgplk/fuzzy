4f2fde4c19cb4e62f1a3225391337ea3351d0ee2
rfgplk <xcfp@protonmail.com>
2024-10-10 14:16:51 +0200

    Append BSL license comments

--- src/fuzzy.hpp
+++ src/fuzzy.hpp
@@ -1,3 +1,10 @@
+
+
+//          Copyright David Lucius Severus 2024-.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 #pragma once
 
 #include "hamming.hpp"         // for hamming distance
